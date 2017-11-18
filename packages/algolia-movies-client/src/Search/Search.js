d02a50e93caf94708b61123d1a42aad8e1f1f068
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2017-11-18 17:51:04 +0100

    Sync/restore search state to url

--- packages/algolia-movies-client/src/Search/Search.js
+++ packages/algolia-movies-client/src/Search/Search.js
@@ -1,4 +1,7 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import queryString from "query-string";
+
 import SearchInput from "../SearchInput";
 import MovieList from "../MovieList";
 import Pagination from "../Pagination";
@@ -6,7 +9,7 @@ import moviesService from "../movies.service";
 
 import "./Search.css";
 
-class Search extends PureComponent {
+class Search extends Component {
   state = { movies: [], currentPage: 0, totalPages: 0, searchText: "" };
 
   _handleSearchChange = searchText => {
@@ -34,7 +37,15 @@ class Search extends PureComponent {
   };
 
   componentDidMount = () => {
-    this._updateSearchResults("", 0);
+    const queryParams = queryString.parse(this.props.location.search);
+    const searchText = queryParams.query || "";
+    const currentPage = queryParams.page ? parseInt(queryParams.page, 10) : 0;
+    // Init the state from the query params.
+    this.setState({ searchText, currentPage });
+  };
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    return this.state !== nextState;
   };
 
   componentWillUpdate = (nextProps, nextState) => {
@@ -44,6 +55,16 @@ class Search extends PureComponent {
     ) {
       this._updateSearchResults(nextState.searchText, nextState.currentPage);
     }
+
+    const queryParams = {
+      page: nextState.currentPage,
+      query: nextState.searchText,
+    };
+    this.props.history.push({ search: `?${queryString.stringify(queryParams)}` });
+  };
+
+  componentWillUnmount = () => {
+    console.log("bye cruel world");
   };
 
   render() {
@@ -72,5 +93,10 @@ class Search extends PureComponent {
     );
   }
 }
+
+Search.propTypes = {
+  history: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+};
 
 export default Search;
